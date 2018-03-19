c5d36782cae9fa0bafe2953671c7b6cbe325c780
Heba ElGalad <31630358+HebaElGalad@users.noreply.github.com>
2018-03-19 16:30:16 +0200

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,10 @@
+$('.btn').on('click', showContent);
+
+function showContent(event) {
+	event.stopPropagation();
+	var clickedElement = $(event.target);
+	if (clickedElement.is('h1')) {
+		clickedElement.next().toggle("100");
+	}
+	console.log(event);
+}
